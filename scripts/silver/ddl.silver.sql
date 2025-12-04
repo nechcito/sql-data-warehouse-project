dc15e21e4006cb1f7b37f0ef37316cce7e8ac8bc
nechcito <peter.meliska77@gmail.com>
2025-12-04 14:06:57 +0100

    Add GO statements after table definitions in SQL script

--- scripts/silver/ddl.silver.sql
+++ scripts/silver/ddl.silver.sql
@@ -12,7 +12,7 @@ cst_gndr NVARCHAR(50),
 cst_created_date DATE,
 dwh_created_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info(
@@ -25,7 +25,7 @@ prd_star_dt DATETIME,
 prd_end_dt DATETIME,
 dwh_created_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details(
@@ -40,7 +40,7 @@ sls_quantity INT,
 sls_price INT,
 dwh_created_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 IF OBJECT_ID ('silver.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12(
@@ -49,7 +49,7 @@ bdate DATE,
 gen NVARCHAR(50),
 dwh_created_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 IF OBJECT_ID ('silver.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101(
@@ -57,7 +57,7 @@ cid NVARCHAR(50),
 cntry NVARCHAR(50),
 dwh_created_date DATETIME2 DEFAULT GETDATE()
 );
-
+GO
 IF OBJECT_ID ('silver.erp_px_cat_g1v2','U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
 CREATE TABLE silver.erp_px_cat_g1v2(
